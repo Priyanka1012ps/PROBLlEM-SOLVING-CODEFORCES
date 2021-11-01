7a71901291dff87139d50519a0a4207ca5283d3a
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2021-11-01 12:11:45 +0530

    Create anton.cpp

---
+++ anton.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+ {
+     int i,n;
+     string s;
+     cin>>s;
+     n=s.length();
+ for(i=0;i<n;i++)
+     {
+          if(ch!='{' && ch!='}' && ch!=" ")
+         {
+          t++;
+         }
+     }
+         
+     for( char j=='a';j<='z';j++)
+      {
+     for(i=0;i<n;i++)
+     {
+         char ch = s[i];;
+         if(ch!='{' && ch!='}' && ch!=" ")
+         {
+             
+                 if(ch==j)
+                 {
+                     f++;
+                 }
+         }
+      }
+      if(f>1)
+      
+     }
+ }
