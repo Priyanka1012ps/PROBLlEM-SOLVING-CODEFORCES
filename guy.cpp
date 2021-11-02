e4cbe6ef5ed3a0901f604217c1513889ff7adfad
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2021-11-02 23:19:18 +0530

    Create guy.cpp

---
+++ guy.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,a;
+    vector <int> p;
+    vector <int> q;
+    for(i=0;i<p.size();i++)
+    {
+        cin>>a;
+         p.push_back(a);
+        
+    }
+    for(i=0;i<q.size();i++)
+    {
+        
+          cin>>a;
+        q.push_back(i);
+    }
+}
